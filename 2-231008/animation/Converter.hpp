cd5c6256ea9163a88408c64762d4624f3b40df8e
yan2u <1955997092@qq.com>
2023-10-15 22:20:48 +0800

    Optimize algorithm

--- 2-231008/animation/Converter.hpp
+++ 2-231008/animation/Converter.hpp
@@ -29,5 +29,22 @@ public:
         pixel_pos_ex /= pixel_pos_ex[2];
         return pixel_pos_ex.block(0, 0, 2, 1);
     }
+
+    void
+    to_pixel_pos(const std::vector<Eigen::Vector3d>& world_pos_array, Eigen::MatrixXd& result) {
+        int n = world_pos_array.size();
+        auto&& world_pos_array_noconst = const_cast<std::vector<Eigen::Vector3d>&>(world_pos_array);
+
+        Eigen::Map<Eigen::MatrixXd> map(world_pos_array_noconst.data()->data(), 3, n);
+        Eigen::MatrixXd world_pos_mat_ex(4, n);
+        world_pos_mat_ex.block(0, 0, 3, n) = map;
+        world_pos_mat_ex.block(3, 0, 1, n).setConstant(1.);
+
+        result = intrinsics_mat * (extrinsics_mat_ex * world_pos_mat_ex);
+
+        auto&& last_row = result.block(2, 0, 1, n).array();
+        result.block(0, 0, 1, n).array() /= last_row;
+        result.block(1, 0, 1, n).array() /= last_row;
+    }
 };
 } // namespace rm
